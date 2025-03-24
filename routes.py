f53fda0a68c7e9e6c709873bf6804fc362ae8c6b
jaVillena17 <javillena13@gmail.com>
2025-03-24 12:53:00 +0100

    changed into MethodView

--- routes.py
+++ routes.py
@@ -1,14 +1,20 @@
 from flask import Flask, render_template, request
-from flask.views import View
+from flask.views import MethodView
 app = Flask(__name__)
 '''
 @app.route('/hello', endpoint='hello')
 def helloWorld():
     return "Hello World!"
 '''
-class HelloView(View):
-    def dispatch_request(self):
-        return "Hola Mundo"
+class HelloView(MethodView):
+    def get(self):
+        return "Hola Mundo!"
+    def post(self):
+        return "Hola Mundo!"
+    def put(self):
+        return "Hola Mundo!"
+    def delete(self):
+        return "Has borrado el Mundo! POR FIN!"
 
 app.add_url_rule('/hello', view_func=HelloView.as_view('hello'))
 
@@ -17,10 +23,16 @@ def helloWorld():
     name = request.args.get('name')
     return "Hola "+ str(name) + "!"'''
 
-class nameView(View):
-    def dispatch_request(self):
+class nameView(MethodView):
+    def get(self):
         name = request.args.get('name')
         return "Bienvenido, " + name
+    def post(self):
+        return "Hola Mundo!"
+    def put(self):
+        return "Hola Mundo!"
+    def delete(self):
+        return "Te has borrado! POR FIN!"
 
 app.add_url_rule('/name', view_func=nameView.as_view('name'))
 
@@ -30,9 +42,14 @@ def index():
     nombre = request.args.get('name')
     return render_template('index.html', name=nombre)'''
 
-class renderView(View):
-    def dispatch_request(self):
+class renderView(MethodView):
+    def get(self):
         nombre = request.args.get('name')
         return render_template('index.html', name=nombre)
-
+    def post(self):
+        return "Hola Mundo!"
+    def put(self):
+        return "Hola Mundo!"
+    def delete(self):
+        return "Te has borrado! POR FIN!"
 app.add_url_rule('/render', view_func=renderView.as_view('render'))
