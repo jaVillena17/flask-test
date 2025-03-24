2c7d58cf92b6a8e2327ea8bfc94f1a594d93d54a
jaVillena17 <javillena13@gmail.com>
2025-03-24 12:05:19 +0100

    borrar archivos unificados en routes.py

--- helloname.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from flask import request
-app = Flask(__name__)
-@app.route('/', endpoint='helloname', methods=['GET'])
-def helloWorld():
-    name = request.args.get('name')
-    return "Hola "+ str(name) + "!"
--- holamundorender.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask, render_template, request
-app = Flask(__name__)
-@app.route('/', endpoint='render')
-def index():
-    nombre = request.args.get('name')
-    return render_template('index.html', name=nombre)
